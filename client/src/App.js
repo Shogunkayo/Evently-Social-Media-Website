0a68fa7e08da0d9f612dddab41ce188d53e4bed8
Gautham Atreyas <89745662+Shogunkayo@users.noreply.github.com>
2022-10-26 21:05:26 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from './home';
+
+function App() {
+  return(
+    <div className='app'>
+        <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeBottom from './home_bottom';
+import HomeTop from './home_top';
+const Home = () => {
+
+    return (  
+        <div className="home">
+            <HomeTop />
+            <HomeBottom />
+        </div>
+    );
+}
+ 
+export default Home;
